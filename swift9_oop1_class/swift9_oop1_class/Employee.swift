a4b6421898d0f8d2057f489a8b94032166f032fa
Tarkan Kara <unnahmet23@hotmail.com>
2023-04-15 21:19:28 +0300

    first class

---
+++ swift9_oop1_class/swift9_oop1_class/Employee.swift
@@ -0,0 +1,16 @@
+//
+//  Employee.swift
+//  swift9_oop1_class
+//
+//  Created by Tarkan Kara on 15.04.2023.
+//
+
+import Foundation
+
+class Employee {
+    
+    var name = ""
+    var age = 0
+    var subject = ""
+    var salary = 0
+}
---
+++ swift9_oop1_class/swift9_oop1_class/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  swift9_oop1_class
+//
+//  Created by Tarkan Kara on 15.04.2023.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var ali = Employee()
+
+ali.name = "Ali"
+ali.age = 25
+ali.subject = "Yazılım"
+ali.salary = 1500
+
+
+ 
